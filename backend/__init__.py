636e166eb650348b565b0857676a88a3ffd88b3c
matheusrafael <matheusrafael@wareline.com.br>
2025-07-16 01:44:25 -0300

    adiciona docker compose

--- backend/__init__.py
+++ backend/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     app = Flask(__name__)
     db_path = os.getenv('SQLITE_PATH')
     db_abspath = os.path.abspath(db_path)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_abspath
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_abspath}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
-from backend import create_app
+from . import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
